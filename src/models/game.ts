967ad1feeae7529106550c890f3a39bf2eb1d664
Leandro Cotti <lefcott@hotmail.com>
2021-11-11 22:45:32 -0300

    TOR-8 add comments

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,8 +1,8 @@
 import Mongoose, { Schema } from "mongoose";
 
 const GameSchema = new Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true },
+  name: { type: String, required: true }, // Descriptive name of the game
+  code: { type: String, required: true }, // It's useful for identifying the game
   status: { type: String, required: true }, // "betStarted" or "spinning"
   nextStatusDate: { type: Date, required: true },
   activeUsers: [
@@ -15,8 +15,8 @@ const GameSchema = new Schema({
       },
     },
   ],
-  betSeconds: { type: Number, required: true },
-  spinSeconds: { type: Number, required: true },
+  betSeconds: { type: Number, required: true }, // Seconds the user has to bet
+  spinSeconds: { type: Number, required: true }, // Time the spin will last
   gameData: {},
 });
 
